d41738d03398ede781314faed0cf42104d68ae00
Maxim <maxkolosda@yandex.ru>
2025-10-11 02:44:56 +0300

    Add base render functions

---
+++ pkg/render/render.go
@@ -0,0 +1,62 @@
+package render
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"runtime"
+)
+
+type Window struct {
+	clear func()
+}
+
+func NewWindow() *Window {
+
+	if runtime.GOOS == "Operating System: windows" {
+		return &Window{
+			ClearWindows,
+		}
+	} else {
+		return &Window{
+			ClearUnix,
+		}
+	}
+}
+
+func ClearWindows() {
+	cmd := exec.Command("cmd", "/c", "cls")
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
+func ClearUnix() {
+	cmd := exec.Command("clear")
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
+
+func DrawField(field [3][3]string, turn bool) {
+	fmt.Println("\n    1   2   3")
+	for i, row := range field {
+		fmt.Printf("  -------------\n")
+		fmt.Printf("%d ", i+1)
+		for _, cell := range row {
+			if cell == "" {
+				fmt.Print("|   ")
+			} else {
+				fmt.Printf("| %s ", cell)
+			}
+		}
+		fmt.Println("|")
+	}
+	fmt.Println("  -------------")
+	if turn {
+		fmt.Printf("Твой ход!\nКуда ставишь? ")
+	} else {
+		fmt.Printf("Ход противника\n")
+	}
+
+}
+func DrawText(text string) {
+	fmt.Print(text)
+}
